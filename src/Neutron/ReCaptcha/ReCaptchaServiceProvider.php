10112b91b60b3c00f7a7ec5150bee3d38187a1ac
Jean-Yves Gaulier <gaulier@alchemy.fr>
2018-03-21 17:02:41 +0100

    Silex2

--- src/Neutron/ReCaptcha/ReCaptchaServiceProvider.php
+++ src/Neutron/ReCaptcha/ReCaptchaServiceProvider.php
@@ -2,19 +2,21 @@
 
 namespace Neutron\ReCaptcha;
 
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+
 
 class ReCaptchaServiceProvider implements ServiceProviderInterface
 {
-    public function register(Application $app)
+    public function register(Container $app)
     {
         $app['recaptcha.public-key'] = null;
         $app['recaptcha.private-key'] = null;
 
-        $app['recaptcha'] = $app->share(function (Application $app) {
+        $app['recaptcha'] = function (Application $app) {
            return ReCaptcha::create($app['recaptcha.public-key'], $app['recaptcha.private-key']);
-        });
+        };
     }
 
     public function boot(Application $app)
