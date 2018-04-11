087f64d14689d1911a7f5d09323832a09908387f
jygaulier <gaulier@alchemy.fr>
2018-04-11 18:56:13 +0200

    Silex2 (#1)
    Silex2
    todo: fix tests

--- src/Neutron/ReCaptcha/ReCaptchaServiceProvider.php
+++ src/Neutron/ReCaptcha/ReCaptchaServiceProvider.php
@@ -2,22 +2,20 @@
 
 namespace Neutron\ReCaptcha;
 
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+
 
 class ReCaptchaServiceProvider implements ServiceProviderInterface
 {
-    public function register(Application $app)
+    public function register(Container $app)
     {
         $app['recaptcha.public-key'] = null;
         $app['recaptcha.private-key'] = null;
 
-        $app['recaptcha'] = $app->share(function (Application $app) {
+        $app['recaptcha'] = function (Application $app) {
            return ReCaptcha::create($app['recaptcha.public-key'], $app['recaptcha.private-key']);
-        });
-    }
-
-    public function boot(Application $app)
-    {
+        };
     }
 }
